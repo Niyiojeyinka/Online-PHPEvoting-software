c81055cd0147f9962baff172ed12ec4b72942ff3
niyiojeyinka <olaniyiojeyinka@gmail.com>
2019-10-23 05:08:55 +0100

    refector the initial.php file

--- application/controllers/Initial.php
+++ application/controllers/Initial.php
@@ -9,8 +9,8 @@ class Initial extends CI_Controller {
     {
 
     $this->load->database();
-
-        $sql1 = "CREATE TABLE users (
+    $queries =array(
+"CREATE TABLE users (
             id int(11) NOT NULL AUTO_INCREMENT,
             firstname varchar(128),
             lastname varchar(128),
@@ -25,11 +25,10 @@ class Initial extends CI_Controller {
             position int(2),
             time int(20),
             PRIMARY KEY (id)
-    );";
-    //slogan not more than 244characters 
+    );",
+  //slogan not more than 244characters 
     //username for candidates
-
-    $sql2 = "CREATE TABLE votes (
+"CREATE TABLE votes (
             id int(11) NOT NULL AUTO_INCREMENT,
             voters_id varchar(128) NOT NULL,
             candidate_id varchar(128) NOT NULL,
@@ -37,31 +36,22 @@ class Initial extends CI_Controller {
             status enum('valid','invalid'),
             time int(20),
             PRIMARY KEY (id)
-    );";
-
-
-
- $sql3= "CREATE TABLE system_var (
+    );",
+    "CREATE TABLE system_var (
     id int(11) NOT NULL AUTO_INCREMENT,
     variable_name varchar(128),
     variable_value varchar(128),
     long_value text,
     PRIMARY KEY (id)
-);";
-
-
-
-
- $sql4= "CREATE TABLE positions (
+);",
+"CREATE TABLE positions (
     id int(11) NOT NULL AUTO_INCREMENT,
     label varchar(128),
     short_form varchar(128),
     PRIMARY KEY (id)
-);";
+);",
 /*release_result, election_start,election_end*/ 
-
-
- $sql6 = "CREATE TABLE mail_mails (
+"CREATE TABLE mail_mails (
     id int(11) NOT NULL AUTO_INCREMENT,
     sender varchar(128),
     receiver varchar(128),
@@ -70,20 +60,19 @@ class Initial extends CI_Controller {
     unique_id varchar(128),
     time varchar(128),
     PRIMARY KEY (id)
-);";
+);"
+    );
 
-
-
- $sqls = array($sql1,$sql2,$sql3,$sql4,$sql5);
+     
 $count = 0;
- foreach($sqls as $sql)
+ foreach($queries as $sql)
  {
  $handle_table_creation = $this->db->query($sql);
  $count++;
   if ($handle_table_creation)
   {
 
-  echo "Table ".$count." sucessfully created"."<br>";
+  echo "Query ".$count." sucessfully executed"."<br>";
 
   }
   }
